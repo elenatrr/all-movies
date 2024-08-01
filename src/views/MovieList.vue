264d7eb79f1a71c5f1efcb652cbd9d53fb2421a7
Elena Tropanets <93614798+elenatrr@users.noreply.github.com>
2024-08-01 22:05:31 +0100

    add categories for filtering the movies

--- src/views/MovieList.vue
+++ src/views/MovieList.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="container mx-auto px-4">
-    <h1 class="text-2xl mb-6 text-center">Popular today</h1>
+    <h1 class="text-2xl mb-4 text-center font-bold">What to watch</h1>
+    <div class="flex justify-center mb-6 gap-4">
+      <button
+        v-for="cat in categories"
+        :key="cat"
+        @click="setCategory(cat)"
+        :class="[
+          'px-2 py-1 rounded-full border-2 border-white hover:border-slate-200',
+          { 'bg-slate-200 border-slate-200': category === cat }
+        ]"
+      >
+        #{{ cat.replace('_', ' ') }}
+      </button>
+    </div>
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
       <MovieTile v-for="movie in movies" :key="movie.id" :movie="movie" />
     </div>
@@ -11,19 +24,27 @@
 <script lang="ts">
 import MovieTile from '@/components/MovieTile.vue'
 import { Movie } from '@/common/definitions'
-import { defineComponent, onMounted, ref } from 'vue'
+import { defineComponent, onMounted, ref, watch } from 'vue'
 import { getMoviesData } from '@/api/axios'
+import { useRoute, useRouter } from 'vue-router'
 
 export default defineComponent({
   name: 'MovieList',
   components: { MovieTile },
 
   setup() {
+    const route = useRoute()
+    const router = useRouter()
     const movies = ref<Movie[]>([])
     const currentPage = ref(1)
     const totalPages = ref(1)
     const isLoading = ref(false)
-    const category = ref('popular')
+    const category = ref(route.query.category || 'popular')
+    const categories = ['popular', 'top_rated', 'now_playing']
+
+    const setCategory = (selectedCategory: string) => {
+      router.push({ name: 'MovieList', query: { category: selectedCategory } })
+    }
 
     const fetchMovies = async (page = 1) => {
       if (isLoading.value || currentPage.value > totalPages.value) {
@@ -33,7 +54,7 @@ export default defineComponent({
       isLoading.value = true
 
       try {
-        const moviesData = await getMoviesData(category.value, page)
+        const moviesData = await getMoviesData(String(category.value), page)
         if (page === 1) {
           movies.value = moviesData.results
         } else {
@@ -54,12 +75,17 @@ export default defineComponent({
       }
     }
 
+    watch(route, () => {
+      category.value = route.query.category || 'popular'
+      fetchMovies()
+    })
+
     onMounted(() => {
       fetchMovies()
       window.addEventListener('scroll', loadMoreMovies)
     })
 
-    return { movies, isLoading, fetchMovies }
+    return { movies, category, setCategory, isLoading, fetchMovies, categories, route }
   }
 })
 </script>
