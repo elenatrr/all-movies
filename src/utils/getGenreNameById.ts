8ac5cb0c66eb78964c9182efe60fa712af00cf1c
Elena Tropanets <93614798+elenatrr@users.noreply.github.com>
2024-08-01 22:24:06 +0100

    add the list of genres for each movie tile

--- src/utils/getGenreNameById.ts
+++
@@ -1,6 +0,0 @@
-import { Genre } from '@/common/definitions'
-
-export function getGenreNameById(genres: Genre[], id: number) {
-  const genre = genres.find((genre) => genre.id === id)
-  return genre ? genre.name : null
-}
